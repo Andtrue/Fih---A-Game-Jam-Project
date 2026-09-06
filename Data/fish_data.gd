2fe0122952770394d1d1775f20b7ceaaa2a2a5e1
MichaelAngulo3232 <michaelangulo3232@gmail.com>
2026-09-06 14:34:33 -0700

    Add FishData resource and 4 test fish

---
+++ Data/fish_data.gd
@@ -0,0 +1,9 @@
+class_name FishData
+extends Resource
+
+@export var fish_name: String = ""
+@export var difficulty: float = 0.0
+@export var min_cast_distance: float = 0.0
+@export var max_cast_distance: float = 100.0
+@export var catch_weight: float = 1.0
+@export var sell_value: int = 0
