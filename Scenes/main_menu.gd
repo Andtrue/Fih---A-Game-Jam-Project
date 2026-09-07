3190c3c3246a4164e58c4b749546017af9d1a491
Will Nguyen <wnguyen745@gmail.com>
2026-09-06 17:41:49 -0700

    Fixed main menu and added events

--- Scenes/main_menu.gd
+++ Scenes/main_menu.gd
@@ -8,4 +8,4 @@ func _ready() -> void:
 
 
 func _on_start_pressed() -> void:
-	get_tree().change_scene_to_file("res://VGDA Fall 5-Day Game Jam/Scenes/Fishing.tscn")
+	get_tree().change_scene_to_file("res://Scenes/Fishing.tscn")
