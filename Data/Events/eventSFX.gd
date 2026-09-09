d4a82d8534a598403429b769220fa3bd430f307a
pi765 <wnguyen745@gmail.com>
2026-09-09 13:59:37 -0700

    Added default orange event popup and laughing cat image

--- Data/Events/eventSFX.gd
+++ Data/Events/eventSFX.gd
@@ -1,8 +1,10 @@
 extends AcceptDialog
 
+const CAT_LAUGH_SFX: AudioStream = preload("res://Assets/SFX/Cat Laughing At You 4.mp3")
+
 const SFX: Array[AudioStream] = [
+	CAT_LAUGH_SFX,
 	preload("res://Assets/SFX/Awww.wav"),
-	preload("res://Assets/SFX/Cat Laughing At You 4.mp3"),
 	preload("res://Assets/SFX/Family Guy Seagull.mp3"),
 	preload("res://Assets/SFX/Idiot.wav"),
 	preload("res://Assets/SFX/Laugh (genuine) 1.wav"),
@@ -24,6 +26,8 @@ const SFX: Array[AudioStream] = [
 
 @onready var sfx_player: AudioStreamPlayer = $AudioStreamPlayer
 
+const CAT_TEXTURE: Texture2D = preload("res://Assets/Event Textures/cat-laughing-4.webp")
+
 func _ready():
 	play_random_sfx()
 
@@ -32,8 +36,19 @@ func play_random_sfx() -> void:
 		return
 
 	# Select a random sound.
-	sfx_player.stream = SFX.pick_random()
-
+	var selected_sfx: AudioStream = SFX.pick_random()
+	sfx_player.stream = selected_sfx
+	
+	if selected_sfx == CAT_LAUGH_SFX:
+		apply_cat_texture()
+	
 	# Play the selected sound.
 	sfx_player.play()
 	
+func apply_cat_texture():
+		var background := StyleBoxTexture.new()
+		background.texture = CAT_TEXTURE
+		add_theme_stylebox_override("panel", background)	# apply background texture	
+		
+		var image_size: Vector2i = Vector2i(CAT_TEXTURE.get_size() * 0.25)
+		size = image_size
