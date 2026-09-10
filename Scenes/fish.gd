14c18793cd28b5b9e19ea6eec704f1c11bab8fab
MichaelAngulo3232 <michaelangulo3232@gmail.com>
2026-09-09 23:37:34 -0700

    Comment out difficulty scaling, fix empty tween in fish.gd

--- Scenes/fish.gd
+++ Scenes/fish.gd
@@ -11,9 +11,9 @@ func _ready() -> void:
 func _on_timer_timeout() -> void:
 	var direction = randi_range(-1, 1)
 	var target_position = position
-	var t = create_tween()
 
 	target_position.y += direction * move_distance
 
 	if direction != 0:
+		var t = create_tween()
 		t.tween_property(self, "position:y", clamp(target_position.y, -25, 25), move_time)
